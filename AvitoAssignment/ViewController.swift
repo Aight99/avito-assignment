7b05d1e88e749386037178539ae6afb095e44bde
ilchenkoal <ltyrepj2001@gmail.com>
2023-08-25 13:29:19 +0300

    Initial commit

---
+++ AvitoAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoAssignment
+//
+//  Created by Александр Ильченко on 25.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+
+
+}
+
